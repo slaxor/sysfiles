cf603da14f5d9fd297d1e61568804134927e0cbd
Sascha Teske <sascha.teske@gmail.com>
2013-03-01 03:05:57 +0100

    Remove debian apt; Add key adding function to installer

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ do_install() {
   sudo /etc/init.d/nginx restart
   sudo /etc/init.d/dnsmasq restart
   sudo restart mongodb
+  rbenv install 1.9.3-p392 --keep
 }
 
 clean() {
@@ -22,10 +23,18 @@ pull() {
   git submodule foreach git checkout master
 }
 
+add_apt_keys() {
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A040830F7FAC5991 #"Google, Inc. Linux Package Signing Key <linux-packages-keymaster@google.com>"
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E0F72778C4676186 #"PlayOnLinux Packaging (PlayOnLinux packaging keys) <packages@playonlinux.com>"
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F24AEA9FB05498B7 #"Valve Corporation <linux@steampowered.com>"
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4BD736A82B5C1B00 #"Sylvain Lebresne (pcmanus) <sylvain@datastax.com>"
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B5B7720097BB3B58 #"Emdebian Archive Signing Key"
+
 success_msg() {
   echo 'I have copied all files to their approproate places'
 }
 
 pull
 do_install
+add_apt_keys
 success_msg
